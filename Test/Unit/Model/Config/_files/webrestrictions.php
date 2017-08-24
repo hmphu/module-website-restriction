d73b9111a31a3a8a34d68eece813411b1f82b308
Phu Hoang <hmp@framco.se>
2017-08-24 10:37:06 +0700

    Init

---
+++ Test/Unit/Model/Config/_files/webrestrictions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+return [
+    'generic' => [
+        0 => 'path_one',
+    ],
+    'register' => [
+        0 => 'path_two',
+    ]
+];
